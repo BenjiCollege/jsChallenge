6d843157a8c6a99c3d2bcc5f6a8dcb7672e7d596
Gerardo Colegio <bctenorsax007@gmail.com>
2022-11-06 20:59:58 -0600

    added notes

---
+++ jschallenge.js
@@ -0,0 +1,14 @@
+// Write a function that takes a number and returns true if it is a positive number and false if it is a negative number.
+
+
+// Write a function that takes a number of days and converts it into an age.
+
+
+// Write a function that takes three numbers and returns the largest of the three numbers.
+
+
+// Write a function that takes an array of names and returns the last name from the array of names.
+
+
+// Write a function that takes an array of numbers and returns true if all of the numbers are positive. It should return false if there are one or more negative numbers in the array.
+
